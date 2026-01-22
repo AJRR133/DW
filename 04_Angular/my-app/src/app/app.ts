fbe5a01c5c50c5de2591e0cb045ac54260d92d60
AJRR133 <arenrod133@g.educaand.es>
2026-01-22 14:27:35 +0100

    22/1

--- 04_Angular/my-app/src/app/app.ts
+++ 04_Angular/my-app/src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hero } from './hero';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+ selector: 'app-root',
+ imports: [RouterOutlet],
+ templateUrl: './app.html',
+ styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+ protected readonly title = signal('my-app');
+
+  getHeroes(){
+     let heroes = [
+       new Hero(1, 'Spiderman'),
+       new Hero(13, 'Wonder Woman'),
+       new Hero(15, 'Lobezno'),
+       new Hero(20, 'Catwoman')
+     ];
+     return heroes;
+ }
 }
+
